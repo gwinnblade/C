551da4706494fbc9b94d02bfa58e80cb91bf1064
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-02-18 23:14:52 +1000

    Add files via upload

---
+++ laba0/lab2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    double Matrix[3][3];
+    double DSumMain = 0;
+    double DSumPoboch = 0;
+    int i, j;
+
+    printf("Input Elements (3x3):\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            (scanf("%lf", &Matrix[i][j]) != 1);
+        }
+    }
+    printf("\n");
+    printf("Matrix: \n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%.2f ", Matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+//    for (i = 0; i < 3; i++) {
+//        mainDiagonalSum += doubleMatrix[i][i];
+//    }
+//
+//    for (i = 0; i < 3; i++) {
+//        secondaryDiagonalSum += doubleMatrix[i][2 - i];
+//    }
+}
+
