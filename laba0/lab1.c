be2f3c40f09b8a84e41cb87f7eea52bf366e23dd
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-02-18 11:22:17 +1000

    Add files via upload

--- laba0/lab1.c
+++ laba0/lab1.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(int argc, char** argv) {
-	double a,b,c;
-	double x1,x2,d;
-	
-//	a = 1.2;
-//	b = 10;
-//	c = 3.3;
-	
-	printf("Input A: \n");
-	scanf("%lf",&a);
-	printf("Input B: \n");
-	scanf("%lf",&b);
-	printf("Input C: \n");
-	scanf("%lf",&c);
-	
-	d = pow(b,2) - 4*a*c;
-	printf("d = %lf \n",d);
-	
-	if (d > 0){
-		x1 = -b + sqrt(d)/(2*a);
-		x2 = -b - sqrt(d)/(2*a);
-		
-		printf("x1 = %lf \n", x1);
-		printf("x2 = %lf \n", x2);
-	}
-	
-	if (d==0){
-		x1 = -b + sqrt(d)/(2*a);
-		printf("x = %lf \n", x1);
-	}
-	
-	if (d < 0) printf("Null");
-}	
+int main() {
+    double a, b, c;
+    double x1, x2, d;
+    char choice;
+    
+    do {
+        printf("Input A: \n");
+        scanf("%lf", &a);
+        printf("Input B: \n");
+        scanf("%lf", &b);
+        printf("Input C: \n");
+        scanf("%lf", &c);
 
+        d = pow(b, 2) - 4*a*c;
+        printf("d = %lf \n", d);
+
+        if (d > 0) {
+            x1 = -b + sqrt(d) / (2*a);
+            x2 = -b + sqrt(d) / (2*a);
+            printf("x1 = %lf \n", x1);
+            printf("x2 = %lf \n", x2);
+        } else if (d == 0) {
+            x1 = -b + sqrt(d) / (2*a);
+            printf("x = %lf \n", x1);
+        } else {
+            printf("Null.\n");
+        }
+
+        printf("Continue? (Y/N): ");
+        scanf(" %c", &choice);
+
+    } while (choice == 'Y' || choice == 'y');
+    return 0;
+}
 
