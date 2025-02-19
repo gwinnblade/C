79e8a51caec82d2579f0407993203a393f5a3e4d
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-02-19 15:58:28 +1000

    Add files via upload

---
+++ laba0/lab2.2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int Matrix[2][2];
+    int MatrixSQ[2][2] = {0};
+    int i, j, k;
+
+    printf("Input Elements (2x2):\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            scanf("%d", &Matrix[i][j]);
+        }
+    }
+
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            MatrixSQ[i][j] = 0;
+            for (k = 0; k < 2; k++) {
+                MatrixSQ[i][j] += Matrix[i][k] * Matrix[k][j];
+            }
+        }
+    }
+
+    printf("Square Martix:\n");
+    for (i = 0; i < 2; i++) {
+        for (j = 0; j < 2; j++) {
+            printf("%d ", MatrixSQ[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
+
