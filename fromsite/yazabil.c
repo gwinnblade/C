42e95204635fa4b513ab0956bb389ec8f81b1334
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-03-06 21:25:44 +1000

    Create yazabil.c

---
+++ fromsite/yazabil.c
@@ -0,0 +1,27 @@
+
+
+// 1)
+// int a = 1;
+// int b = 2;
+// int c = a;
+// a = b;
+// b = c;
+
+// 2)
+// int a = 1;
+// int b = 2;
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// 3)
+// int a = 1;
+// int b = 2;
+// a = a ^ b;
+// b = a ^ b;
+// a = a ^ b;
+
+// 4)
+// int a = 1;
+// int b = 2;
+// a = a+b-(b=a)
