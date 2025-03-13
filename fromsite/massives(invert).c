e7d637e2ca61a95c520d6ca775d3a5c7b5a3de56
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-03-13 23:39:18 +1000

    Create massives(invert).c

---
+++ fromsite/massives(invert).c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int numbers[] = {1, 2, 3, 4, 5, 6};
+
+    int n = sizeof(numbers) / sizeof(numbers[0]);
+    int m = n / 2;
+    for(int i = 0; i < m; i++)
+    {
+        int temp = numbers[i];
+        numbers[i] = numbers[n-i-1];
+        numbers[n-i-1] = temp;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        printf("%d", numbers[i]);
+    }
+    return 0;
+}
