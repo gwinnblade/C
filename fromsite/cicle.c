e872081c2ca8a6466e677d511c4b98a6206dc6ec
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-03-13 22:12:36 +1000

    Create cicle.c

---
+++ fromsite/cicle.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+  
+int main(void)
+{
+    for(int i=0; i < 5; i++)
+    {
+        printf("for - Hello\n");
+    }
+ 
+    int c = 0;
+    while(c < 5)
+    {
+        printf("while - Hello\n");
+        c++;
+    }
+    return 0;
+}
