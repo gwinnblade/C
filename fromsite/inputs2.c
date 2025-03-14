3578b8e0937a03cd8dc54411ed90238d2744e0ce
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-03-14 20:28:16 +1000

    Create inputs2.c

---
+++ fromsite/inputs2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int age;
+    char name[20];
+    printf("Your age: ");
+    scanf("%d", &age);
+    printf("Your name: ");
+    scanf(" %20[^\n]", name);
+    printf("Name: %s \t Age = %d\n", name, age);
+    return 0;
+}
