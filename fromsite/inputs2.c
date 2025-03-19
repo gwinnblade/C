ce3031c12c150d5b4c3085286684b4d71362487a
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-03-19 23:08:03 +1000

    Update inputs2.c

--- fromsite/inputs2.c
+++ fromsite/inputs2.c
@@ -11,3 +11,34 @@ int main(void)
     printf("Name: %s \t Age = %d\n", name, age);
     return 0;
 }
+
+
+
+//Дана следующая программа:
+
+/*
+
+#include <stdio.h>
+  
+int main(void)
+{
+    int age;
+    char name[20];
+    // вводим возраст
+    printf("Your age: ");
+    scanf("%d", &age);
+    // вводим имя
+    printf("Your name: ");
+    scanf("%20s", name);
+     
+    printf("Name: %s \t Age = %d",name,  age);
+    return 0;
+}
+
+*/
+
+/* Эта программа имеет проблему - при вводе имени в переменную name помещается только первая часть имени, которая идет до пробела:
+Your age: 34
+Your name: Tom Smith
+Name: Tom        Age = 34
+Исправьте программу таким образом, чтобы она могла принимать в качестве имени строки, разделенные пробелом, например, "Tom Smith". */
