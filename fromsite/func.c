787d52142bb850349a5c01811d8149e76c57bc38
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-03-25 21:39:30 +1000

    Create func.c

---
+++ fromsite/func.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void hello_world()
+{
+    printf("agxnyagxnyagxnyagxnyagxny\n");
+}
+int main(void)
+{
+    hello_world();
+    return 0;
+}
+
+// Определите и вызове функцию, которую назовите hello_world и которая будет выводить на консоль строку "Hello World!".
