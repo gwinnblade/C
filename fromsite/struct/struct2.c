214a9c6dad3d2c5833902b50ba3733dd1ba7d0ae
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-04-01 21:14:56 +1000

    Add files via upload

---
+++ fromsite/struct/struct2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+typedef struct
+{
+	char *name;
+	int age;
+	double salary;
+} employee;
+
+int main(void)
+{
+	employee sanya = {"Sanya", 22, 5252};
+	employee vanya = {"Vanya", 23, 2525};
+	printf("Name %s \t Age %d \t Salary %.2f\n", sanya.name, sanya.age, sanya.salary);
+	printf("Name %s \t Age %d \t Salary %.2f\n", vanya.name, vanya.age, vanya.salary);
+	return 0;
+}
