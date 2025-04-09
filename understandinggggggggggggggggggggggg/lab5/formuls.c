3b058dbf9531406ab78f5c14129a5fd9d7bba17e
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-04-09 20:38:48 +1000

    Add files via upload

---
+++ understandinggggggggggggggggggggggg/lab5/formuls.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// * - указатель 
+double* matrix_operation(double* matrix1, double* matrix2, int n, char operation){
+// malloc (memory allocation (выделение памяти)) - выделение динамической памяти
+// sizeof - размер в байтах
+	double* result = (double*)malloc(n*n*sizeof(double));
+	
+	if(operation == '+') {
+		for (int i = 0; i<n*n; i++) {
+			result[i] = matrix1[i] + matrix2[i];
+		}
+	} else if (operation == '-') {
+		for (int i = 0; i<n*n; i++) {
+			result[i] = matrix1[i] - matrix2[i];
+		}
+	} else if (operation == '*') {
+		for (int i = 0; i<n; i++){
+			for (int j = 0; j<n; j++){
+				result[i*n + j] = 0;
+				for (int k = 0; k < n; k++){
+					result[i*n + j] += matrix1[i*n + k] * matrix2[k*n + j];
+				}
+			}
+		}
+	} else {
+		return NULL;
+	}
+	return result;
+}
---
+++ understandinggggggggggggggggggggggg/lab5/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double* matrix_operation(double* matrix1, double* matrix2, int n, char operation);
+
+int main(){
+	int n;
+	char operation;
+	
+	printf("Input size of matrix N: ");
+	scanf("%d", &n);
+	
+	double* matrix1 = (double*)malloc(n*n*sizeof(double));
+	double* matrix2 = (double*)malloc(n*n*sizeof(double));
+	
+	printf("Input elements of 1st matrix:\n");
+	for(int i = 0; i<n*n; i++){
+		scanf("%lf", &matrix1[i]);
+	}
+	printf("Input elements of 2nd matrix:\n");
+	for(int i = 0; i<n*n; i++){
+		scanf("%lf", &matrix2[i]);
+	}
+	
+	printf("operation (+ | - | *): ");
+	scanf(" %c", &operation);
+	
+	double* result = matrix_operation(matrix1, matrix2, n, operation);
+	
+	printf("Result: \n");
+	for(int i = 0; i<n; i++){
+		for(int j = 0; j<n; j++){
+			printf("%lf ", result[i*n + j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
