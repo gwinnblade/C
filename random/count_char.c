15404313d6f1aed7427b5d630c141bfbf9c2c884
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-04-16 23:01:39 +1000

    Update count_char.c

--- random/count_char.c
+++ random/count_char.c
@@ -5,12 +5,12 @@ void count_chars(const char *str) {
     int counts[256] = {0};
 
     for (int i = 0; str[i]; i++) {
-        char c = tolower(str[i]);
+        char c = tolower(str[i]); // переводит заглавные в строковые
         counts[(unsigned char)c]++;
     }
 
     for (int i = 0; i < 256; i++) {
-        if (counts[i] > 0 && isprint(i)) {
+        if (counts[i] > 0 && isprint(i)) { // проверка на печатные символы а не управляющий (пример \n) 
             printf("'%c': %d\n", i, counts[i]);
         }
     }
