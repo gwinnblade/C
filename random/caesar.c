291bcc7ea67dd6fa8634a6549c23e1b635a7cd4e
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-04-16 22:57:23 +1000

    Update caesar.c

--- random/caesar.c
+++ random/caesar.c
@@ -6,11 +6,11 @@
 #include <string.h>
 
 void caesar_encrypt(char *str, int shift) {
-    shift %= 26;
+    shift %= 26; //Если дает остаток значит сдвиг правильно введен
     for (int i = 0; str[i]; i++) {
-        if (isupper(str[i])) {
-            str[i] = 'A' + (str[i] - 'A' + shift + 26) % 26;
-        } else if (islower(str[i])) {
+        if (isupper(str[i])) { // isupper возвращает True если буква заглавная
+            str[i] = 'A' + (str[i] - 'A' + shift + 26) % 26; // Идет подсчет какая буква будет
+        } else if (islower(str[i])) { // islower возвращает True если буква строчная
             str[i] = 'a' + (str[i] - 'a' + shift + 26) % 26;
         }
     }
