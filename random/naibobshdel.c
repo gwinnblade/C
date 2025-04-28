554b6659920564cd482e0f6a161ed05968161156
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-04-28 23:48:09 +1000

    Create naibobshdel.c

---
+++ random/naibobshdel.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int main() {
+    int num1, num2;
+    printf("Input 2 numbers: ");
+    scanf("%d %d", &num1, &num2);
+
+    int result = gcd(num1, num2);
+    printf("Naibolsh Obsh Del %d and %d: %d\n", num1, num2, result);
+
+    return 0;
+}
