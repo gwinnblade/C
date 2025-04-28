b35fa6a35f3c253665a8f11a9e3ebfdba450b4f2
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-04-28 23:55:06 +1000

    Create count_words_in_txt.c

---
+++ random/count_words_in_txt.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int countWords(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error of open file\n");
+        return -1;
+    }
+    int wordCount = 0;
+    int inWord = 0;
+    char c;
+    while ((c = fgetc(file)) != EOF) {
+        if (isspace(c)) {
+            if (inWord) {
+                wordCount++;
+                inWord = 0;
+            }
+        } else {
+            inWord = 1;
+        }
+    }
+    if (inWord) {
+        wordCount++;
+    }
+
+    fclose(file);
+    return wordCount;
+}
+int main() {
+    const char* filename = "text.txt";
+    int words = countWords(filename);
+    printf("Count of words in file: %d\n", words);
+    return 0;
+}
