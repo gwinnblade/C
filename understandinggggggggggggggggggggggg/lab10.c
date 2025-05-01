ce6966e3e8ed1f2b96eaf3bf2d5533c605608386
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-05-01 22:45:04 +1000

    Add files via upload

---
+++ understandinggggggggggggggggggggggg/lab10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_NUM(arr, len, result)	\
+	result = arr[0];				\
+	for(int i = 1; i < len; i++) {	\
+		if (arr[i] > result)		\
+			result =arr[i];			\
+	}
+
+int main() {
+	char input[100];
+	int nums[20];
+	int c = 0;
+	
+	printf("Input numbers: ");
+	fgets(input, 100, stdin);
+	
+	char* token = strtok(input, " ");
+	while (token != NULL && c < 20) {
+		nums[c] = atoi(token);
+		c++;
+		token = strtok(NULL, " ");
+	}
+	
+	int max;
+	MAX_NUM(nums, c, max);
+	printf("Max Num: %d\n", max);
+}
