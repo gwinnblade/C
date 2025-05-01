b6ca6b035c36956e6f2df65bcead71281e6a3d95
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-05-01 23:12:00 +1000

    Add files via upload

---
+++ lab10/operation.h
@@ -0,0 +1,10 @@
+#ifndef MYMATH_H
+#define MYMATH_H
+
+double main_sum(int count, ...);
+double main_max(int count, ...);
+double main_min(int count, ...);
+double main_avg(int count, ...);
+
+#endif
+
