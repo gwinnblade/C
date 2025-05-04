2f80148fe2c3d5b2445f1905c4d9043b9a4c7c6c
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-05-04 23:15:11 +1000

    Add files via upload

---
+++ random/unique_num.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int num;
+	int digits[10] = {0};
+	
+	printf("Input number ");
+	scanf("%d", &num);
+	
+	while (num > 0) {
+		int d = num % 10;
+		if (digits[d] == 1) {
+			printf("Repeat digit: %d\n", d);
+			return 0;
+		}
+		digits[d] = 1;
+		num /= 10;
+	}
+	printf("All digit uniqie\n");
+	return 0;
+}
