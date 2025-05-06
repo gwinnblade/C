4911225afa10a927da3adf64c42b32086785f593
gwinnblade <46234566+gwinnblade@users.noreply.github.com>
2025-05-06 20:41:56 +1000

    Add files via upload

---
+++ understandinggggggggggggggggggggggg/lab11/operation.h
@@ -0,0 +1,11 @@
+#ifndef OPERATION_H
+#define OPERATION_H
+#include <stdarg.h>
+
+int sum(int count, ...);
+int max(int count, ...);
+int min(int count, ...);
+double average(int count, ...);
+
+#endif // OPERATION_H
+
